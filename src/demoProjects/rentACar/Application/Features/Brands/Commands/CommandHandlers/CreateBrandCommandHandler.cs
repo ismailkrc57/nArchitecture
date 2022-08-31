2016c147db66f92810a77e4fe6801acff0a58a96
ismailkrc57 <mustafaciceknote8@gmail.com>
2022-08-31 21:16:23 +0300

    Initial commit

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CommandHandlers/CreateBrandCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Features.Brands.Commands.CreateBrand;
+using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.CommandHandlers;
+
+public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandDto>
+{
+    private readonly IBrandRepository _repository;
+    private readonly IMapper _mapper;
+    private readonly BrandBusinessRules _businessRules;
+
+    public CreateBrandCommandHandler(IBrandRepository repository, IMapper mapper, BrandBusinessRules businessRules)
+    {
+        _repository = repository;
+        _mapper = mapper;
+        _businessRules = businessRules;
+    }
+
+
+    public async Task<CreatedBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+    {
+        await _businessRules.BrandNameCanNotBeDuplicatedWhenInserted(request.Name);
+
+        Brand mappedBrand = _mapper.Map<Brand>(request);
+        Brand createdBrand = await _repository.AddAsync(mappedBrand);
+        CreatedBrandDto createdBrandDto = _mapper.Map<CreatedBrandDto>(createdBrand);
+
+        return createdBrandDto;
+    }
+}
---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,9 @@
+﻿using Application.Features.Brands.Dtos;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.CreateBrand;
+
+public class CreateBrandCommand : IRequest<CreatedBrandDto>
+{
+    public string Name { get; set; }
+}
